a82f0083a18ab018a48aaf3ee10034c936cf2801
NaoEdoJose <naoedojose.nave@gmail.com>
2015-03-07 21:46:21 -0300

    Calculator
    A simple calculator used for a school work

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+
+        public double add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double sub(double num1, double num2)
+        {
+
+            return num1 - num2;
+        }
+
+        public double multi(double num1, double num2)
+        {
+
+            return num1 * num2;
+        }
+
+        public double div(double num1, double num2)
+        {
+
+            return num1 / num2;
+        }
+
+        public double sqrt(double num1)
+        {
+
+            return Math.Sqrt(num1);
+        }
+
+        public double porce(double num1, double num2)
+        {
+
+            return num1 / 100 * num2;
+        }
+
+        public double restDiv(double num1, double num2)
+        {
+
+            return num1 % num2;
+        }
+
+        public double poten(double num1, double num2)
+        {
+            return Math.Pow(num1, num2);
+        }
+
+        public double sin(double num1) {
+
+            return Math.Sin(num1);
+        }
+
+        public double cos(double num1)
+        {
+
+            return Math.Cos(num1);
+        }
+
+        public double tan(double num1)
+        {
+
+            return Math.Tan(num1);
+        }
+    }
+}
---
+++ Calculator/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Calculator;
+using System.Collections;
+
+namespace Calculator
+{
+    public partial class Calculadora : Form
+    {
+        string operation;
+        bool conta_ready = false;
+        double num1 = 0;
+        double num2 = 0;
+        Calculator calc;
+
+        public Calculadora()
+        {
+            InitializeComponent();
+            Box.Text = "0";
+            calc = new Calculator();
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        private void numClick(object sender, EventArgs e)
+        {
+            if (conta_ready || Box.Text == "0")
+                Box.Clear();
+
+            conta_ready = false;
+            Button NumClick = (Button)sender;
+            Box.Text = Box.Text + NumClick.Text;
+            operationShow.Text = operationShow.Text + NumClick.Text;
+
+        }
+
+        private void operatorClick(object sender, EventArgs e)
+        {
+            num2 = 0;
+            num1 = 0;
+            if (num1 != 0)
+                num2 = Double.Parse(Box.Text);
+
+            Button b = (Button)sender;
+            operation = b.Text;
+            num1 = Double.Parse(Box.Text);
+            operationShow.Text = operationShow.Text + b.Text;
+
+            if (num1 != 0 && num2 != 0)
+            {
+                switch (operation)
+                {
+                    case "+":
+                        num2 = Double.Parse(Box.Text);
+                        Box.Text = Convert.ToString(calc.add(num1, num2));
+                        num1 = 0;
+                        num2 = 0;
+                        break;
+                    case "-":
+                        num2 = Double.Parse(Box.Text);
+                        Box.Text = Convert.ToString(calc.sub(num1, num2));
+                        num1 = 0;
+                        num2 = 0;
+                        break;
+                    case "/":
+                        num2 = Double.Parse(Box.Text);
+                        Box.Text = Convert.ToString(calc.div(num1, num2));
+                        num1 = 0;
+                        num2 = 0;
+                        break;
+                    case "*":
+                        num2 = Double.Parse(Box.Text);
+                        Box.Text = Convert.ToString(calc.multi(num1, num2));
+                        num1 = 0;
+                        num2 = 0;
+                        break;
+                    case "%":
+                        num2 = Double.Parse(Box.Text);
+                        Box.Text = Convert.ToString(calc.porce(num1, num2));
+                        num1 = 0;
+                        num2 = 0;
+                        break;
+                    case "R":
+                        num2 = Double.Parse(Box.Text);
+                        Box.Text = Convert.ToString(calc.restDiv(num1, num2));
+                        num1 = 0;
+                        num2 = 0;
+                        break;
+                    case "^":
+                        num2 = Double.Parse(Box.Text);
+                        Box.Text = Convert.ToString(calc.poten(num1, num2));
+                        num1 = 0;
+                        num2 = 0;
+                        break;
+                }
+                conta_ready = false;
+            }
+
+            if (num1 != 0)
+            {
+                switch (operation)
+                {
+                    case "sen":
+                        Box.Text = Convert.ToString(calc.sin(num1));
+                        num1 = 0;
+                        break;
+                   
+                    case "cos":
+                        Box.Text = Convert.ToString(calc.cos(num1));
+                        num1 = 0;
+                        break;
+                   
+                    case "tan":
+                        Box.Text = Convert.ToString(calc.tan(num1));
+                        num1 = 0;
+                        break;
+                  
+                    case "√":
+                        Box.Text = Convert.ToString(calc.sqrt(num1));
+                        num1 = 0;
+                        break;
+                }
+                conta_ready = false;
+            }
+            conta_ready = true;
+        }
+
+        private void equalsClick(object sender, EventArgs e)
+        {
+            if (num1 != 0)
+            {
+                switch (operation)
+                {
+                    case "+":
+                        num2 = Double.Parse(Box.Text);
+                        Box.Text = Convert.ToString(calc.add(num1, num2));
+                        num1 = 0;
+                        num2 = 0;
+                        break;
+                    case "-":
+                        num2 = Double.Parse(Box.Text);
+                        Box.Text = Convert.ToString(calc.sub(num1, num2));
+                        num1 = 0;
+                        num2 = 0;
+                        break;
+                    case "/":
+                        num2 = Double.Parse(Box.Text);
+                        Box.Text = Convert.ToString(calc.div(num1, num2));
+                        num1 = 0;
+                        num2 = 0;
+                        break;
+                    case "*":
+                        num2 = Double.Parse(Box.Text);
+                        Box.Text = Convert.ToString(calc.multi(num1, num2));
+                        num1 = 0;
+                        num2 = 0;
+                        break;
+                    case "por":
+                        num2 = Double.Parse(Box.Text);
+                        Box.Text = Convert.ToString(calc.porce(num1, num2));
+                        num1 = 0;
+                        num2 = 0;
+                        break;
+                    case "%":
+                        num2 = Double.Parse(Box.Text);
+                        Box.Text = Convert.ToString(calc.restDiv(num1, num2));
+                        num1 = 0;
+                        num2 = 0;
+                        break;
+                    case "^":
+                        num2 = Double.Parse(Box.Text);
+                        Box.Text = Convert.ToString(calc.poten(num1, num2));
+                        num1 = 0;
+                        num2 = 0;
+                        break;
+                }
+                conta_ready = false;
+            }
+        }
+
+        private void clearClick(object sender, EventArgs e)
+        {
+            Box.Text = "";
+            operationShow.Text = string.Empty;
+        }
+
+        private void Calculadora_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
